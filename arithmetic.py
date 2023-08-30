5c04ec45df2ad24178a5a0f3629d7de6b4e2a10b
JK <jkay@Justin.>
2023-08-30 12:23:14 -0700

    add multi

--- arithmetic.py
+++ arithmetic.py
@@ -32,3 +32,13 @@ def power(num1, num2):
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
     return num1%num2
+
+def add_mult(num1, num2, num3):
+    """Get the sum of num1 and num2, then multiply sum with num3."""
+    sum = num1 + num2
+    num = sum * num3
+    return num
+
+def add_cubes(num1, num2):
+    """Add the cubes of num1 and num2."""
+    
